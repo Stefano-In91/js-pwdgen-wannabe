dade6fd6187f1c0f2741c3742311594c508c3a65
Stefano.In91 <ste.ince91@gmail.com>
2022-10-19 13:14:52 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Nome > Cognome > Colore > Console.log > innherHtml = psw
+
+let userName = prompt("Ciao Bomber, qual'è il tuo nome?");
+let userSurname = prompt("E il tuo cognome?");
+let userColor = prompt("Qual'è il tuo colore preferito?");
+
+console.log(userName, userSurname, userColor);
+
+document.getElementById("app-input").innerHTML = `La tua Very nice password è ${userName} ${userSurname} ${userColor} 76`;
+
+// Uguale ma Array
+
+const userArray = [];
+
+userArray.push( prompt("Il tuo nome?") );
+userArray.push( prompt("Il tuo cognome?") );
+userArray.push( prompt("Il tuo colore preferito?") );
+
+console.log(userArray);
+
+document.getElementById("array-input").innerHTML = `La tua Really nice password è ${userArray[0]} ${userArray[1]} ${userArray[2]} 76`;
